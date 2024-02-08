7f6f853a919c6791ceaf09940811cf77fb020797
Nimna Niwarthana <niwarthananimna@gmail.com>
2024-02-08 16:22:26 +0530

    🔨 Set up fastapi for malditectist

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="MalDitectist",
+    description="FastAPI for MalDitectist",
+    version="2.0.0"
+)
+
+# Root route
+@app.get("/", tags=['Root'])
+def home():
+    return {'message': 'Welcome to MalDitectist'}
